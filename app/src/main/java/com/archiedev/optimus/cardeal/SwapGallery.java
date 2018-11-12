14772a1dd4aab3b5d122d44ec86d8b2d9f8bb160
Unknown <oktawianlasek@gmail.com>
2018-11-12 20:07:25 +0100

    change from kotlin to java

---
+++ app/src/main/java/com/archiedev/optimus/cardeal/SwapGallery.java
@@ -0,0 +1,13 @@
+package com.archiedev.optimus.cardeal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SwapGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swap_gallery);
+    }
+}
